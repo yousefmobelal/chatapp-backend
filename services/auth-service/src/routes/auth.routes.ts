05462f71df20960fcbc74edeb181b992aa21d7b0
yousefmobelal <ym8370302@gmail.com>
2026-02-17 13:54:29 +0200

    feat(auth-service): add refresh and revoke token handlers to auth routes

--- services/auth-service/src/routes/auth.routes.ts
+++ services/auth-service/src/routes/auth.routes.ts
@@ -1,4 +1,9 @@
-import { loginHandler, registerHandler } from '@/controllers/auth.controller';
+import {
+  loginHandler,
+  refreshHandler,
+  registerHandler,
+  revokeTokenHandlers,
+} from '@/controllers/auth.controller';
 import { validateRequest } from '@chatapp/common';
 import { Router } from 'express';
 import { loginSchema, refreshSchema, registerSchema, revokeSchema } from '@/routes/auth.schema';
@@ -7,5 +12,5 @@ export const authRouter: Router = Router();
 
 authRouter.post('/register', validateRequest({ body: registerSchema.shape.body }), registerHandler);
 authRouter.post('/login', validateRequest({ body: loginSchema.shape.body }), loginHandler);
-authRouter.post('/refresh', validateRequest({ body: refreshSchema.shape.body }), loginHandler);
-authRouter.post('/revoke', validateRequest({ body: revokeSchema.shape.body }), loginHandler);
+authRouter.post('/refresh', validateRequest({ body: refreshSchema.shape.body }), refreshHandler);
+authRouter.post('/revoke', validateRequest({ body: revokeSchema.shape.body }), revokeTokenHandlers);
